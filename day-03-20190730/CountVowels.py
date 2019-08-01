d2848336e7c7f89e8169f131fe522a48ec5091ea
kopair <32586709+kopair@users.noreply.github.com>
2019-08-01 18:07:36 +0800

    python03-2

--- day-03-20190730/CountVowels.py
+++ day-03-20190730/CountVowels.py
@@ -4,4 +4,7 @@ from collections import Counter
 
 num = input('Enter a String:')
 common = Counter(num).most_common()
-#print(Counter(num).most_common())
+for i in 'aeuoi':
+    for (item,index) in common:
+        if i==item:
+            print('{}: {}'.format(item,str(index)))
--- day-03-20190730/readme.md
+++ day-03-20190730/readme.md
@@ -1,1 +1,2 @@
-ReverseString.py  ---> Reverse a String - 输入一个字符串，程序将反转它并打印出来
+ReverseString.py  ---> Reverse a String - 输入一个字符串，程序将反转它并打印出来<br>
+CountVowels.py ---> 计数元音 - 输入一个字符串，程序计算文本中元音的数量。为了增加复杂性，它会报告找到的每个元音的总和。
