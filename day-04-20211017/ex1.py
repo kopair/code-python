6e25f19e6d338219a38ba2f8f03daf9cd16e5229
kopair <32586709+kopair@users.noreply.github.com>
2021-10-18 11:28:47 +0800

    第四天
    学习print、注释、运算

---
+++ day-04-20211017/ex1.py
@@ -0,0 +1,9 @@
+print("Hello World")
+# python 识别不了中文字符
+print("测试！")
+# 安装好了python3之后可以识别中文字符
+print('asd')
+
+# 学会使用#注释
+print('This is run!')
+# print("This isn't run!")
---
+++ day-04-20211017/ex3.py
@@ -0,0 +1,9 @@
+# 变量
+cars = 1
+car_12 = 2
+car_sd = 3
+sad = car_12 * car_sd
+print(cars)
+print(car_12)
+print(car_sd)
+print(sad)
