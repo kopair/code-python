0f81b0d945095e639b6a20eac87e5495dc75d10b
kopair <32586709+kopair@users.noreply.github.com>
2021-10-20 00:24:09 +0800

    第六天
    学习了输入和导入模块

---
+++ day-06-20211019/ex7.py
@@ -0,0 +1,18 @@
+# 输入
+print("How old are you ?", end="  ")
+age = input()
+
+print("How tall are u?", end="  ")
+height = input()
+
+print(f"you age is {age}, {height} tall")
+
+# 整数输入
+print("input a intnum:")
+num = int(input())
+print(f"output:{num}")
+
+# input值里面加入提示
+age = input("your age")
+height = input("your height")
+print(f"age {age}, height{height}")
---
+++ day-06-20211019/ex8.py
@@ -0,0 +1,3 @@
+from sys import argv
+script, first, second, third = argv
+print(script, first, second, third)
